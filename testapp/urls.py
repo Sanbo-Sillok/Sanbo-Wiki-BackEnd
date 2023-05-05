eccffab79cbf472b0d3f53df48cee9e934122fd1
yang <hc1489@naver.com>
2023-05-06 00:55:04 +0900

    패키지 추가 설치

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,6 +4,5 @@ from testapp.views import *
 urlpatterns = [
     path('', get_testdata_all, name='get_testdata_all'),
     path('new', create_testdata, name='create_testdata'),
-    path('<str:title>', get_testdata,  name='get_testdata'),
-    
+    path('<str:title>', get_testdata,  name='get_testdata'),    
 ]
