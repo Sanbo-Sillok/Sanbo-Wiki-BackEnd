e1a3378c26382b05ac9629f2ed1fd9e2d95bfde2
yang <hc1489@naver.com>
2023-05-11 15:06:54 +0900

    local IP 추가

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://172.30.1.9:3000",
 ]
 
 ROOT_URLCONF = 'config.urls'
