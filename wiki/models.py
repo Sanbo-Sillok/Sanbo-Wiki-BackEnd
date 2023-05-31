142eba854419e1eab67ad268f4dda18b2a65f162
yang <hc1489@naver.com>
2023-05-31 16:38:43 +0900

    model field 수정

--- wiki/models.py
+++ wiki/models.py
@@ -16,4 +16,4 @@ class Post(models.Model):
     status = models.CharField(verbose_name="상태", choices=CHOICES, default='ACTIVE', max_length=20)
     
     created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
-    update_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
+    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
