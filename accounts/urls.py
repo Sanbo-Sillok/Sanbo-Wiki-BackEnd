3843cb32a7830246cb699c58d72938fcf0cc7b59
yang <hc1489@naver.com>
2023-08-07 11:31:39 +0900

    토큰 api 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [ 
     path('signup', RegisterView.as_view()),
     path('login', AuthView.as_view()),
+    # 토큰
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
