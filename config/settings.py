ccc26a606b8730d35b890cb745154168b5d66ba5
yang <hc1489@naver.com>
2023-08-07 18:39:10 +0900

    디버그 상태 변경

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ pymysql.install_as_MySQLdb()
 SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
